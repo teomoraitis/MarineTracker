c1a6eecebdfd28ba506f5e93d7cef6d701083766
“Theodoros <theodorosmoraitis2002@gmail.com>
2025-03-29 15:45:31 +0200

    Backend springboot initialized

---
+++ Backend-SpringBoot/src/main/java/com/di/marinetracker/backendspringboot/BackendSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.di.marinetracker.backendspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringBootApplication.class, args);
+        System.out.println("Starting the Backend for the MarineTracker web platform...");
+
+    }
+
+}
