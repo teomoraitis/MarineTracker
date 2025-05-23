0c6230f16ec277d0670c450914badad5e30dc340
plerros <48536508+plerros@users.noreply.github.com>
2025-05-23 22:55:24 +0300

    backend initial user entity

---
+++ Backend-SpringBoot/src/main/java/com/di/marinetracker/backendspringboot/entities/User.java
@@ -0,0 +1,59 @@
+package com.di.marinetracker.backendspringboot.entities;
+
+import jakarta.persistence.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private String id;
+    private String userName;
+    private String hashedPassword;
+    private String role;
+
+
+    @OneToMany(mappedBy = "mmsi", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OrderBy("timestamp DESC")
+    private List<Vessel> fleet = new ArrayList<>();
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    public User(String userName, String password, String role) {
+        this.userName = userName;
+        this.hashedPassword = passwordEncoder().encode(password);
+        this.role = role;
+    }
+
+    public User() {
+    }
+
+    public String getId() {return id;}
+
+    public String getUserName() {return userName;}
+
+    public void setPassword(String password) { this.hashedPassword = passwordEncoder().encode(password); }
+
+    public String getRole() {return role;}
+
+    public boolean passwordMatch(String password) {
+        return (passwordEncoder().matches(password, this.hashedPassword));
+    }
+
+    public String toString() {
+        return "User{" +
+                "id=" + id + '\'' +
+                "userName=" + userName + '\'' +
+                "role=" + role + '\'' +
+            '}';
+    }
+}
