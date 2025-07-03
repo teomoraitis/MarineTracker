b7bdefcfae6a7ae7eb52454a33988bd3deaec650
sdi2000150 <sdi2000150@di.uoa.gr>
2025-07-03 15:48:20 +0300

    merged tests implemented so far

--- Backend-SpringBoot/src/test/java/com/di/marinetracker/backendspringboot/services/VesselPositionsConsumerTest.java
+++ Backend-SpringBoot/src/test/java/com/di/marinetracker/backendspringboot/services/VesselPositionsConsumerTest.java
@@ -1,49 +1,95 @@
-//package com.di.marinetracker.backendspringboot.services;
-//
-//import com.di.marinetracker.backendspringboot.repositories.VesselRepository;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.messaging.simp.SimpMessagingTemplate;
-//
-//import java.util.Optional;
-//
-//import static org.mockito.Mockito.*;
-//
-//@ExtendWith(MockitoExtension.class)
-//class VesselPositionsConsumerTest {
-//    @Mock
-//    SimpMessagingTemplate messagingTemplate;
-//
-//    @Mock
-//    VesselRepository vesselRepository;
-//
-//    @InjectMocks
-//    VesselPositionsConsumer consumer;
-//
-//    private final ObjectMapper objectMapper = new ObjectMapper();
-//
-//    @Test
-//    void testConsume_validJson_sendsMessage() throws Exception {
-//        String kafkaMessage = "{\"mmsi\":\"123456789\",\"type\":\"Cargo\"}";
-//        String expectedMessage = objectMapper.readTree(kafkaMessage).toPrettyString();
-//        when(vesselRepository.findById(any())).thenReturn(Optional.empty());
-//
-//        consumer.consume(kafkaMessage);
-//
-//        verify(messagingTemplate, times(1))
-//                .convertAndSend("/topic/locations", expectedMessage);
-//    }
-//
-//    @Test
-//    void testConsume_invalidJson_doesNotSend() {
-//        String invalidJson = "{invalid json}";
-//
-//        consumer.consume(invalidJson);
-//
-//        verify(messagingTemplate, never()).convertAndSend(anyString(), any(Object.class));
-//    }
-//}
+package com.di.marinetracker.backendspringboot.services;
+
+import com.di.marinetracker.backendspringboot.entities.Vessel;
+import com.di.marinetracker.backendspringboot.entities.VesselPosition;
+import com.di.marinetracker.backendspringboot.repositories.VesselPositionRepository;
+import com.di.marinetracker.backendspringboot.repositories.VesselRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class VesselPositionsConsumerTest {
+    @Mock
+    VesselRepository vesselRepository;
+
+    @Mock
+    VesselPositionRepository vesselPositionRepository;
+
+    @Mock
+    WebSocketService webSocketService;
+
+    @InjectMocks
+    VesselPositionsConsumer consumer;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    void testInvalidJson_silentlyIgnored() {
+        consumer.consume("{invalid json}");
+        verifyNoInteractions(webSocketService);
+    }
+
+    @Test
+    void testValidJson_sendsMessage() throws Exception {
+        String kafkaMessage = "{\"mmsi\":\"123456789\",\"type\":\"Cargo\",\"lat\":0,\"lon\":0,\"speed\":0,\"course\":0,\"status\":0,\"turn\":0,\"heading\":0,\"timestamp\":0}";
+        when(vesselRepository.findById(any())).thenReturn(Optional.of(new Vessel("123456789", "Cargo")));
+
+        consumer.consume(kafkaMessage);
+
+        verify(webSocketService).broadcastVesselPosition(eq(kafkaMessage), any(), any());
+    }
+
+    @Test
+    void testUpdates9MinutesApart_arentSavedTogether() {
+        when(vesselRepository.findById(any())).thenReturn(Optional.of(new Vessel("123456789", "Cargo")));
+        VesselPosition[] lastOne = new VesselPosition[2];
+        // Times: 0, 1 second, 1 minute.
+        // lastOne[0] is the latest saved. lastOne[1] is only there because the first update of a vessel never changes.
+        // We expect the saved 1-second one to go away and the 1-minute one to be saved.
+        lastOne[0] = new VesselPosition(null, 0.0, 0.0, 0.0, 0.0, 0, 0.0, 0, Instant.ofEpochSecond(599));
+        lastOne[1] = new VesselPosition(null, 0.0, 0.0, 0.0, 0.0, 0, 0.0, 0, Instant.ofEpochSecond(0));
+        Mockito.when(vesselPositionRepository.find2LatestByVesselMmsi("123456789")).thenReturn(Optional.of(lastOne));
+        consumer.consume("{\"mmsi\":\"123456789\",\"type\":\"Cargo\",\"lat\":0,\"lon\":0,\"speed\":0,\"course\":0,\"status\":0,\"turn\":0,\"heading\":0,\"timestamp\":601}");
+        verify(vesselPositionRepository, times(1)).delete(any());
+    }
+
+    @Test
+    void testUpdates10MinutesApart_areSavedTogether() {
+        when(vesselRepository.findById(any())).thenReturn(Optional.of(new Vessel("123456789", "Cargo")));
+        VesselPosition[] lastOne = new VesselPosition[2];
+        // 0, 1 second, 1 second + 1 minute + 1 second = 602 seconds
+        lastOne[0] = new VesselPosition(null, 0.0, 0.0, 0.0, 0.0, 0, 0.0, 0, Instant.ofEpochSecond(601));
+        lastOne[1] = new VesselPosition(null, 0.0, 0.0, 0.0, 0.0, 0, 0.0, 0, Instant.ofEpochSecond(0));
+        Mockito.when(vesselPositionRepository.find2LatestByVesselMmsi("123456789")).thenReturn(Optional.of(lastOne));
+        consumer.consume("{\"mmsi\":\"123456789\",\"type\":\"Cargo\",\"lat\":0,\"lon\":0,\"speed\":0,\"course\":0,\"status\":0,\"turn\":0,\"heading\":0,\"timestamp\":602}");
+        verify(vesselPositionRepository, times(0)).delete(any());
+    }
+
+    @Test
+    void testFirstUpdates_areSavedEvenIfTheyAreQuick() {
+        when(vesselRepository.findById(any())).thenReturn(Optional.of(new Vessel("123456789", "Cargo")));
+        VesselPosition[] lastOne = new VesselPosition[1];
+        // Times: 0, 1 second, 1 minute.
+        // lastOne[0] is the latest saved. lastOne[1] is only there because the first update of a vessel never changes.
+        // We expect the saved 1-second one to go away and the 1-minute one to be saved.
+        lastOne[0] = new VesselPosition(null, 0.0, 0.0, 0.0, 0.0, 0, 0.0, 0, Instant.ofEpochSecond(0));
+        Mockito.when(vesselPositionRepository.find2LatestByVesselMmsi("123456789")).thenReturn(Optional.of(lastOne));
+        consumer.consume("{\"mmsi\":\"123456789\",\"type\":\"Cargo\",\"lat\":0,\"lon\":0,\"speed\":0,\"course\":0,\"status\":0,\"turn\":0,\"heading\":0,\"timestamp\":1}");
+        verify(vesselPositionRepository, times(0)).delete(any());
+    }
+
+}
--- Backend-SpringBoot/src/test/java/com/di/marinetracker/backendspringboot/services/WebSocketServiceTest.java
+++ Backend-SpringBoot/src/test/java/com/di/marinetracker/backendspringboot/services/WebSocketServiceTest.java
@@ -1,70 +1,160 @@
-//package com.di.marinetracker.backendspringboot.services;
-//
-//import com.di.marinetracker.backendspringboot.entities.User;
-//import com.di.marinetracker.backendspringboot.entities.Vessel;
-//import com.di.marinetracker.backendspringboot.repositories.UserRepository;
-//import com.di.marinetracker.backendspringboot.utils.JwtUtils;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.*;
-//import org.springframework.messaging.simp.SimpMessagingTemplate;
-//
-//import java.util.*;
-//
-//import static org.mockito.Mockito.*;
-//
-//@ExtendWith(org.mockito.junit.jupiter.MockitoExtension.class)
-//class WebSocketServiceTest {
-//
-//    @Mock
-//    private SimpMessagingTemplate messagingTemplate;
-//    @Mock
-//    private UserRepository userRepository;
-//    @Mock
-//    private JwtUtils jwtUtils;
-//
-//    @InjectMocks
-//    private WebSocketService webSocketService;
-//
-//    @Test
-//    void testBroadcastVesselPosition_sendsToGuestAndAuthenticated() {
-//        Vessel vessel = mock(Vessel.class);
-//        when(vessel.getMmsi()).thenReturn("123456789");
-//        when(vessel.getType()).thenReturn("Cargo");
-//
-//        String vesselMessage = "{\"mmsi\":\"123456789\",\"type\":\"Cargo\"}";
-//
-//        webSocketService.broadcastVesselPosition(vesselMessage, vessel);
-//
-//        verify(messagingTemplate, atLeastOnce()).convertAndSend(anyString(), anyString());
-//    }
-//
-//    @Test
-//    void testRegisterUserSession_validToken_registersSessionAndSendsInitialData() {
-//        String sessionId = "session1";
-//        String jwtToken = "valid.jwt.token";
-//        String userId = "user1";
-//        User user = mock(User.class);
-//        when(jwtUtils.validateJwtToken(jwtToken)).thenReturn(true);
-//        when(jwtUtils.getUserNameFromJwtToken(jwtToken)).thenReturn(userId);
-//        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
-//        when(user.getFleet()).thenReturn(Collections.emptyList());
-//        when(user.getZoneOfInterest()).thenReturn(null);
-//
-//        webSocketService.registerUserSession(sessionId, jwtToken);
-//
-//        verify(userRepository).findById(userId);
-//    }
-//
-//    @Test
-//    void testRemoveUserSession_removesSession() {
-//        String sessionId = "session2";
-//        webSocketService.removeUserSession(sessionId);
-//    }
-//
-//    @Test
-//    void testUpdateUserFilters_updatesFiltersAndSendsFilteredData() {
-//        String sessionId = "session3";
-//        webSocketService.updateUserFilters(sessionId, Set.of("Cargo"));
-//    }
-//}
+package com.di.marinetracker.backendspringboot.services;
+
+import com.di.marinetracker.backendspringboot.controllers.VesselCacheController;
+import com.di.marinetracker.backendspringboot.entities.User;
+import com.di.marinetracker.backendspringboot.entities.Vessel;
+import com.di.marinetracker.backendspringboot.entities.VesselPosition;
+import com.di.marinetracker.backendspringboot.repositories.UserRepository;
+import com.di.marinetracker.backendspringboot.utils.JwtPrincipal;
+import com.di.marinetracker.backendspringboot.utils.JwtUtils;
+import org.checkerframework.checker.optional.qual.Present;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.stubbing.OngoingStubbing;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+import java.security.Principal;
+import java.time.Instant;
+import java.util.*;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(org.mockito.junit.jupiter.MockitoExtension.class)
+class WebSocketServiceTest {
+
+    @Mock
+    private SimpMessagingTemplate messagingTemplate;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private JwtUtils jwtUtils;
+
+    private boolean revalidateJwtWhenUsersRegister = false;
+
+    @InjectMocks
+    private WebSocketService webSocketService;
+    @Mock
+    private VesselCacheController vesselDataCache;
+
+    @Test
+    void testRemoveUserThatDoesntExists_doesntThrow() {
+        webSocketService.removeUserSession("session that doesn't exist");
+    }
+
+    @Test
+    void testBroadcastToNobody_doesNothing() {
+        broadcastSomething();
+        verify(messagingTemplate).convertAndSend(eq("/topic/guest"), anyString());
+        verifyNoMoreInteractions(messagingTemplate);
+    }
+
+    @Test
+    void testRegisterUserAndBroadcastVessel_sendsIt() {
+        createUser("a");
+        broadcastSomething();
+        verify(messagingTemplate).convertAndSendToUser(eq("a"), eq("/queue/vessels"), anyString());
+        verify(messagingTemplate).convertAndSend(eq("/topic/guest"), anyString());
+        verifyNoMoreInteractions(messagingTemplate);
+    }
+
+    @Test
+    void testTwoUsers_bothGetBroadcast() {
+        createUser("a");
+        createUser("b");
+        broadcastSomething();
+        verify(messagingTemplate).convertAndSendToUser(eq("b"), eq("/queue/vessels"), anyString());
+        verify(messagingTemplate).convertAndSendToUser(eq("a"), eq("/queue/vessels"), anyString());
+        verify(messagingTemplate).convertAndSend(eq("/topic/guest"), anyString());
+        verifyNoMoreInteractions(messagingTemplate);
+    }
+
+    @Test
+    void testFilterByNonExistingFleet_noMessage() {
+        createUser("a");
+        createUser("b");
+        ArrayList<String> types = new ArrayList<>();
+        types.add("type-that-doesnt-exist");
+        webSocketService.updateUserFilters("session-of-a", types);
+
+        broadcastSomething();
+        verify(messagingTemplate).convertAndSendToUser(eq("b"), eq("/queue/vessels"), anyString());
+        verify(messagingTemplate).convertAndSend(eq("/topic/guest"), anyString());
+        verifyNoMoreInteractions(messagingTemplate);
+    }
+
+    // TODO the filters are under construction
+    // @Test
+    void testDisablingFilters() {
+        createUser("a");
+        createUser("b");
+        ArrayList<String> types = new ArrayList<>();
+        types.add("type-that-doesnt-exist");
+        webSocketService.updateUserFilters("session-of-a", types);
+        webSocketService.updateFleetFilter("session-of-a", false);
+
+        broadcastSomething();
+        verify(messagingTemplate).convertAndSendToUser(eq("a"), eq("/queue/vessels"), anyString());
+        verify(messagingTemplate).convertAndSendToUser(eq("b"), eq("/queue/vessels"), anyString());
+        verify(messagingTemplate).convertAndSend(eq("/topic/guest"), anyString());
+        verifyNoMoreInteractions(messagingTemplate);
+    }
+
+    @Test
+    void testReenablingFilters() {
+        createUser("a");
+        createUser("b");
+        ArrayList<String> types = new ArrayList<>();
+        types.add("type-that-doesnt-exist");
+        webSocketService.updateUserFilters("session-of-a", types);
+        webSocketService.updateFleetFilter("session-of-a", false);
+        webSocketService.updateFleetFilter("session-of-a", true);
+
+        broadcastSomething();
+        verify(messagingTemplate).convertAndSendToUser(eq("b"), eq("/queue/vessels"), anyString());
+        verify(messagingTemplate).convertAndSend(eq("/topic/guest"), anyString());
+        verifyNoMoreInteractions(messagingTemplate);
+    }
+
+    // TODO the filters are under construction
+    //@Test
+    void testTwoFiltersWhereOneMatches_sendsMessage() {
+        createUser("a");
+        createUser("b");
+        ArrayList<String> types = new ArrayList<>();
+        types.add("type-that-doesnt-exist");
+        types.add("Cargo");
+        webSocketService.updateUserFilters("session-of-a", types);
+
+        broadcastSomething();
+        verify(messagingTemplate).convertAndSendToUser(eq("a"), eq("/queue/vessels"), anyString());
+        verify(messagingTemplate).convertAndSendToUser(eq("b"), eq("/queue/vessels"), anyString());
+        verify(messagingTemplate).convertAndSend(eq("/topic/guest"), anyString());
+        verifyNoMoreInteractions(messagingTemplate);
+    }
+
+    private void createUser(String name) {
+        Principal principal = Mockito.mock(JwtPrincipal.class);
+        Mockito.when(principal.getName()).thenReturn(name);
+        User user = new User(name, "a@a", "password", Set.of());
+        Optional<User> optionalUser = Optional.of(user);
+        when(userRepository.findByUserNameWithFleet(name)).thenReturn(optionalUser);
+        webSocketService.registerUserSession("session-of-" + name, principal, false);
+    }
+
+    private void broadcastSomething() {
+        Vessel vessel = mock(Vessel.class);
+
+        String mmsi = "12345";
+        String type = "Cargo";
+
+        when(vessel.getMmsi()).thenReturn(mmsi);
+
+        String vesselMessage = "{\"mmsi\":\"" + mmsi + "\",\"type\":\"" + type + "\"}";
+        // There would be all the fields in the real JSON. Maybe the code can be written differently.
+
+        VesselPosition sameVesselAsPosition = new VesselPosition(vessel, 0.0, 0.0, 1.0, 0.0, 0, 0.0, 0, Instant.ofEpochSecond(0));
+        webSocketService.broadcastVesselPosition(vesselMessage, vessel, sameVesselAsPosition);
+    }
+
+}
